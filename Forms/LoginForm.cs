25661b2e744d2b1e7776276531b5d110760d9168
stevobain <steven.bainter@outlook.com>
2024-03-28 15:31:37 -0500

    Added custom form to serve as a messagebox to dynamically edit text between english and spanish while the form is open

--- Forms/LoginForm.cs
+++ Forms/LoginForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing;
 using System.Globalization;
 using System.IO;
 using System.Security.Authentication;
@@ -11,11 +13,36 @@ namespace sbCalendar.Forms
         public LoginForm()
         {
             InitializeComponent();
+            SystemEvents.UserPreferenceChanged += SystemEvents_UserPreferenceChanged;
+            CreateErrorForm(errorForm1, textBox1);
+            CreateErrorForm(errorForm2, textBox2);
         }
 
         RegionInfo regionInfo;
         CultureInfo cultureInfo;
         StreamWriter streamWriter;
+
+        Form errorForm1 = new Form();
+        Form errorForm2 = new Form();
+        TextBox textBox1 = new TextBox();
+        TextBox textBox2 = new TextBox();
+
+        public void CreateErrorForm(Form errorForm, TextBox textBox)
+        {            
+            textBox.Multiline = true;
+            textBox.Size = new Size(errorForm.Width - 24, errorForm.Height - 48);
+            textBox.Location = new Point(4, 4);
+            Button button = new Button();
+            button.Text = "OK";
+            button.Location = new Point(110, 200);
+            button.DialogResult = DialogResult.OK;
+            errorForm.Text = "sbCalendar";
+            errorForm.FormBorderStyle = FormBorderStyle.FixedDialog;
+            errorForm.AcceptButton = button;
+            errorForm.StartPosition = FormStartPosition.CenterScreen;
+            errorForm.Controls.Add(button);
+            errorForm.Controls.Add(textBox);
+        }
 
         private void loginButton_Click(object sender, EventArgs e)
         {
@@ -23,16 +50,18 @@ namespace sbCalendar.Forms
             string password = passwordTextBox.Text;
             string timestampFormat = cultureInfo.DateTimeFormat.SortableDateTimePattern;
 
-            // If the username or password textboxes are empty, an error message is displayed using the current UI language (EN, ES)
+            // If the username or password textboxes are empty, an error message is displayed in es-US or en-US
             if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
             {
                 switch (CultureInfo.CurrentCulture.ToString())
                 {
                     case "es-US":
-                        MessageBox.Show("Debes ingresar un nombre de usuario y contraseña para continuar.", "sbCalendar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        textBox1.Text = "Debes ingresar un nombre de usuario y contraseña para continuar.";
+                        errorForm1.ShowDialog();
                         break;
                     default:
-                        MessageBox.Show("You must enter a username and password to continue.", "sbCalendar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        textBox1.Text = "You must enter a username and password to continue.";
+                        errorForm1.ShowDialog();
                         break;
                 }
             }
@@ -40,14 +69,14 @@ namespace sbCalendar.Forms
             {
                 try
                 {
-                    // Try authenticating the user, if successful, write success message to log; if failed, an error message is displayed in the current UI language (EN, ES)
+                    // Try authenticating the user, if successful, write success message to log; if failed, an error message is displayed in es-US or en-US
 
                     ClientScheduleRepository clientScheduleRepository = new ClientScheduleRepository();
                     if (clientScheduleRepository.AuthenticateUser(username, password))
                     {
-                        if (File.Exists("../login.log"))
+                        if (File.Exists("../Login_History.txt"))
                         {
-                            var fileStream = new FileStream("../login.log", FileMode.Append, FileAccess.Write);
+                            var fileStream = new FileStream("../Login_History.txt", FileMode.Append, FileAccess.Write);
                             using (streamWriter = new StreamWriter(fileStream))
                             {
                                 streamWriter.WriteLine($"{DateTime.Now.ToString(timestampFormat)} | {username} has logged in successfully");
@@ -55,7 +84,7 @@ namespace sbCalendar.Forms
                         }
                         else
                         {
-                            var fileStream = new FileStream("../login.log", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                            var fileStream = new FileStream("../Login_History.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
 
                             using (streamWriter = new StreamWriter(fileStream))
                             {
@@ -73,10 +102,12 @@ namespace sbCalendar.Forms
                         switch (CultureInfo.CurrentCulture.ToString())
                         {                            
                             case "es-US":
-                                MessageBox.Show("El nombre de usuario y/o contraseña es incorrecto.", "sbCalendar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                textBox2.Text = "El nombre de usuario y/o contraseña es incorrecto.";
+                                errorForm2.ShowDialog();
                                 break;
                             default:
-                                MessageBox.Show("The username and/or password is incorrect.", "sbCalendar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                textBox2.Text = "The username and/or password is incorrect.";
+                                errorForm2.ShowDialog();
                                 break;
                         }
                     }
@@ -91,7 +122,31 @@ namespace sbCalendar.Forms
 
         private void loginForm_Load(object sender, EventArgs e)
         {
-            // Controls on form load text using current culture
+            // Controls on form load text using current culture and region
+            CultureInfo.CurrentCulture.ClearCachedData();
+            cultureInfo = CultureInfo.CurrentCulture;
+            regionInfo = RegionInfo.CurrentRegion;
+            if (cultureInfo.Name == "es-US")
+            {
+                usernameLabel.Text = "Usuario";
+                passwordLabel.Text = "Contraseña";
+                loginButton.Text = "Acceso";
+                regionLabel.Text = $"Usted está aquí: {regionInfo.NativeName}";
+                infoLabel.Text = "Esta página de inicio de sesión se traduce entre es-US y en-US.";                
+            }
+            else
+            {
+                usernameLabel.Text = "Username";
+                passwordLabel.Text = "Password";
+                loginButton.Text = "Log In";
+                regionLabel.Text = $"You are here: {regionInfo.EnglishName}";
+                infoLabel.Text = "This login page translates between en-US and es-US.";
+            }
+        }
+
+        void SystemEvents_UserPreferenceChanged(object sender, UserPreferenceChangedEventArgs e)
+        {
+            // Controls on form load text using current culture and region
             CultureInfo.CurrentCulture.ClearCachedData();
             cultureInfo = CultureInfo.CurrentCulture;
             regionInfo = RegionInfo.CurrentRegion;
@@ -102,6 +157,8 @@ namespace sbCalendar.Forms
                 loginButton.Text = "Acceso";
                 regionLabel.Text = $"Usted está aquí: {regionInfo.NativeName}";
                 infoLabel.Text = "Esta página de inicio de sesión se traduce entre es-US y en-US.";
+                textBox1.Text = "Debes ingresar un nombre de usuario y contraseña para continuar.";
+                textBox2.Text = "El nombre de usuario y/o contraseña es incorrecto.";
             }
             else
             {
@@ -110,6 +167,8 @@ namespace sbCalendar.Forms
                 loginButton.Text = "Log In";
                 regionLabel.Text = $"You are here: {regionInfo.EnglishName}";
                 infoLabel.Text = "This login page translates between en-US and es-US.";
+                textBox1.Text = "You must enter a username and password to continue.";
+                textBox2.Text = "The username and/or password is incorrect.";
             }
         }
     }
